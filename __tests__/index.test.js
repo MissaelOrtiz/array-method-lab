7c036915a64218cf5fc6f267888baf7171533d64
Missael Ortiz <ortiz.missael80@gmail.com>
2021-07-20 17:17:57 -0700

    added test document with map array method test

---
+++ __tests__/index.test.js
@@ -0,0 +1,12 @@
+describe('array method functions', () => {
+  describe('map array method', () => {
+    it('takes an array and a callback function and creates a new array with the callback function implemented.', () => {
+      const callBackFunc = item => item++ ;
+      const arr = [1, 2, 3];
+      const expected = [2, 3, 4];
+      const actual = fakeMap(arr, callBackFunc);
+
+      expect(expected).toEqual(actual);
+    });
+  });
+});
